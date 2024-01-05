f9c860e30bf4f93222ae1b8fb64923d231b15016
Andrea <gelsogrove@gmail.com>
2024-01-05 15:35:33 +0100

    fine marina

--- Assets/Script/CubeInteraction.cs
+++ Assets/Script/CubeInteraction.cs
@@ -12,36 +12,20 @@ public class CubeInteraction : MonoBehaviour
      
     private void Start()
     {
-           containerObject.SetActive(false);
-           graphGriddObject.SetActive(false);
+        containerObject.SetActive(false);
+        graphGriddObject.SetActive(false);
     }
 
-  public void OpenDetails(GameObject backPlate)
+    public void OpenDetails(GameObject backPlate)
     {
         detailsGridObject.SetActive(!detailsGridObject.activeSelf);
-
         RawImage backPlateRawImage = backPlate.GetComponent<RawImage>();
-
-        if (backPlateRawImage != null)
-        {
-            Debug.Log("RawImage trovata su: " + backPlate.name);
-            
-            // Imposta il colore con valori RGB convertiti
-            if (backPlateRawImage.color == Color.red) {
-                    backPlateRawImage.color =   new Color(151f / 255f, 216f / 255f, 1f, 51f / 255f);
-
-            }else {
-                backPlateRawImage.color = Color.red;
-            }
-            
-
-
-            backPlateRawImage.SetMaterialDirty(); 
+        if (backPlateRawImage.color == Color.red) {
+            backPlateRawImage.color =   new Color(151f / 255f, 216f / 255f, 1f, 51f / 255f);
+        }else {
+            backPlateRawImage.color = Color.red;
         }
-        else
-        {
-            Debug.LogError("RawImage non trovata su: " + backPlate.name);
-        }
+        backPlateRawImage.SetMaterialDirty(); 
     }
 
 
@@ -52,75 +36,54 @@ public class CubeInteraction : MonoBehaviour
     {
         graphGriddObject.SetActive(!graphGriddObject.activeSelf);
     }
-    
 
 
     public void OnTouch()
     {
-      if (containerObject != null)
+        if (containerObject != null && cubeObject != null)
         {
-            
-            containerObject.SetActive(!containerObject.activeSelf);
-            
-            detailsGridObject.SetActive(false);
-            graphGriddObject.SetActive(false);
-
-            // Otteniamo la posizione del cubo principale
+          
+            // Cube position
             Vector3 mainCubePosition = cubeObject.transform.position;
-
-            // Calcoliamo la posizione desiderata  
-            Vector3 desiredPosition = mainCubePosition + new Vector3(0.7f , -0.2f, 0f);
-
-            // Impostiamo la rotazione desiderata
+    
+            // New position
+            Vector3 desiredPosition = mainCubePosition + new Vector3(0.7f, -0.2f, 0f);
             Quaternion desiredRotation = Quaternion.identity;
 
-            // Attiviamo un nuovo metodo per spostare l'oggetto dopo un breve ritardo
-            SetContainerSolution(containerObject, desiredPosition, desiredRotation);
-            
+            // Set values
+            containerObject.transform.position = desiredPosition;
+            containerObject.transform.rotation = desiredRotation;
+
+            // Hide - Show
+            containerObject.SetActive(!containerObject.activeSelf);
+            detailsGridObject.SetActive(false);
+            graphGriddObject.SetActive(false);
         }
         else
         {
             Debug.LogWarning("Oggetto non trovato.");
         }
-
     }
 
-    private void SetContainerSolution(GameObject obj, Vector3 position, Quaternion rotation)
+
+
+
+    public void CloseMainSlate() 
     {
-        if (obj != null)
-        {
-            obj.transform.position = position;
-            obj.transform.rotation = rotation;
-        }
-        else
-        {
-            Debug.LogWarning("Oggetto non valido.");
-        }
-    }
-
-    public void CloseMainSlate() {
         containerObject.SetActive(false);
         detailsGridObject.SetActive(false);
         graphGriddObject.SetActive(false);
     }
 
-    public void CloseDetailsSlate() {
-        
+    public void CloseDetailsSlate() 
+    {    
         detailsGridObject.SetActive(false);
         graphGriddObject.SetActive(false);
     }
 
     public void CloseGraphSlate()
-{
-    // Assicurati che graphGriddObject sia stato inizializzato correttamente
-    if (graphGriddObject != null)
     {
         graphGriddObject.SetActive(false);
     }
-    else
-    {
-        Debug.LogWarning("graphGriddObject non valido.");
-    }
-}
 
 }
