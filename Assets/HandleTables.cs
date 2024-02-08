4868548de47dc95a467c5863cb24cb99c9927fdb
Andrea <andrea_gelsomino@hotmail.com>
2024-02-08 21:39:55 +0100

    first click cube

--- Assets/HandleTables.cs
+++ Assets/HandleTables.cs
@@ -1,60 +1,67 @@
-using UnityEditor;
+using TMPro;
 using UnityEngine;
 
-public class HandleTables : MonoBehaviour
+public class MyClickHandler : MonoBehaviour
 {
     public GameObject Cube;
     public GameObject CubeR;
     public GameObject Title;
     public GameObject Menu;
     public GameObject Container;
+    public TextMeshPro debugTextMesh;
+    public Transform controllerTransform;
+    public float raycastDistance = 10f;
 
-
+    private bool isMenuOpen = false;
 
     void Update()
     {
-        // Assicurati che l'oggetto Title esista prima di procedere
         if (Title != null)
         {
-            // Ottenere la direzione dalla camera dell'utente all'oggetto Title
             Vector3 directionToTitle = Title.transform.position - Camera.main.transform.position;
             Vector3 directionToMenu = Menu.transform.position - Camera.main.transform.position;
-            Vector3 directionToContainer = Container.transform.position - Camera.main.transform.position;
 
-
-            // Calcolare la rotazione necessaria per far puntare l'oggetto Title verso la camera
             Quaternion lookRotationTitle = Quaternion.LookRotation(directionToTitle, Vector3.up);
             Quaternion lookRotationMenu = Quaternion.LookRotation(directionToMenu, Vector3.up);
-            
-
 
             Title.transform.rotation = lookRotationTitle;
             Menu.transform.rotation = lookRotationMenu;
 
-
             CubeR.transform.Rotate(Vector3.up, 90f * Time.deltaTime);
+            CubeR.transform.position = Cube.transform.position;
 
-            CubeR.transform.position  = Cube.transform.position;
+            if (OVRInput.GetDown(OVRInput.Button.Any, OVRInput.Controller.RTouch) && !isMenuOpen)
+            {
+                Ray ray = new Ray(controllerTransform.position, controllerTransform.forward);
+                RaycastHit hit;
 
-
+                if (Physics.Raycast(ray, out hit, raycastDistance))
+                {
+                    if (hit.collider.CompareTag("Cube"))
+                    {
+                        OpenMenu();
+                        isMenuOpen = true;
+                    }
+                }
+            }
+            else if (OVRInput.GetUp(OVRInput.Button.Any, OVRInput.Controller.RTouch))
+            {
+                isMenuOpen = false;
+            }
         }
-
-
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
-        {
-            this.OpenContainer();
-        }
-
-       
-
     }
 
     public void OpenMenu()
     {
-        Cube.transform.Rotate(Vector3.left, 360f);
+        GameObject menu = GameObject.Find("SceneObjects/Case1/Cube/Canvas/Menu");
 
-        GameObject menu = GameObject.FindGameObjectWithTag("Menu");
-        menu?.SetActive(true);
+        if (menu != null)
+        {
+            bool isActive = menu.activeSelf;
+            menu.SetActive(!isActive);
+
+            debugTextMesh.text = "OpenMenu " + !isActive;
+        }
     }
 
     public void OpenContainer()
