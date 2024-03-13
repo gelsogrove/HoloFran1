6212adf4402e30b8d96b750bfb0233471f47e576
Andrea <andrea_gelsomino@hotmail.com>
2024-03-13 18:39:37 +0100

    fix raycast

---
+++ Assets/Scripts/HandPinchDetector.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPinchDetector : MonoBehaviour
+{
+
+    [SerializeField] private HandPointer handPointer;
+    [SerializeField] private AudioClip pinchSound;
+    [SerializeField] private AudioClip releaseSound;
+
+    private bool _hasPinched;
+    private bool _isIndexFingerPinching;
+    private float _pinchStrength;
+    private OVRHand.TrackingConfidence _confidence;
+
+    private void Update() => CheckPInch(handPointer.rightHand);
+    
+
+    void CheckPInch(OVRHand hand)
+    {
+        _pinchStrength = hand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
+        _isIndexFingerPinching = hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+        _confidence = hand.GetFingerConfidence(OVRHand.HandFinger.Index);
+
+        if (handPointer.CurrentTarget)
+        {
+            Material currentMaterial = handPointer.CurrentTarget.GetComponent<Renderer>().material;
+            currentMaterial.SetFloat("_Metallic", _pinchStrength);
+        }
+
+        if (!_hasPinched && _isIndexFingerPinching && _confidence == OVRHand.TrackingConfidence.High && handPointer.CurrentTarget)
+        {
+            _hasPinched = true;
+            handPointer.CurrentTarget.GetComponent<AudioSource>().PlayOneShot(pinchSound);
+        }else if (_hasPinched && !_isIndexFingerPinching)
+        {
+            _hasPinched = false;
+            handPointer.CurrentTarget.GetComponent<AudioSource>().PlayOneShot(releaseSound);
+        }
+
+    }
+
+}
